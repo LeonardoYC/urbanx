546af3cbc6a579739bc35b9d401463499f5ae367
JeanClix <jp10.02.2003@gmail.com>
2024-09-30 12:18:35 -0500

    Producto/Create

--- Models/Producto.cs
+++ Models/Producto.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Diagnostics.CodeAnalysis;
 
-namespace urbanx.Models
+namespace UrbanX.Models
 {
     [Table("t_producto")]
     public class Producto
