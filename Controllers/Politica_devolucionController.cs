678a9ea63d90783b74292b4ac55385f60e617e42
Julio3021 <julio_antaurco@usmp.pe>
2024-09-29 21:45:09 -0500

    controller de politica devolucion

---
+++ Controllers/Politica_devolucionController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace UrbanX.Controllers
+{
+
+    public class Politica_devolucionController : Controller
+    {
+        private readonly ILogger<Politica_devolucionController> _logger;
+
+        public Politica_devolucionController(ILogger<Politica_devolucionController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
