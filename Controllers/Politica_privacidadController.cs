52c5066dd70d7d22454acf6e094b3ea14e6e430a
eduardo0804 <jesuslazbra1008@outlook.com>
2024-09-30 14:07:25 -0500

    Subiendo Política y privacidad

---
+++ Controllers/Politica_privacidadController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace UrbanX.Controllers
+{
+    public class Politica_privacidadController : Controller
+    {
+        private readonly ILogger<Politica_privacidadController> _logger;
+
+        public Politica_privacidadController(ILogger<Politica_privacidadController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
