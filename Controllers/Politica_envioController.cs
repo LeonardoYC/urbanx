761fce7b770f43aaffa89a69b9ab16fd724f829c
Julio3021 <julio_antaurco@usmp.pe>
2024-09-30 23:13:07 -0500

    controller para la politica-envio

---
+++ Controllers/Politica_envioController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace UrbanX.Controllers
+{
+
+    public class Politica_envioController : Controller
+    {
+        private readonly ILogger<Politica_envioController> _logger;
+
+        public Politica_envioController(ILogger<Politica_envioController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
