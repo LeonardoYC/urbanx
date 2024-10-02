a071e98755fd3361c54e1c877b6222d3ac7e981b
Leonardo <leonardoyupan2012@gmail.com>
2024-10-02 00:40:03 -0500

    renombre a ubicacion

--- Controllers/EquipoController.cs
+++ Controllers/EquipoController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+
 namespace urbanx.Controllers
 {
     public class EquipoController : Controller
