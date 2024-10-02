83c075cb31ab38fa183d849d806965605a1e7414
Leonardo <leonardoyupan2012@gmail.com>
2024-10-02 00:41:51 -0500

    renombres a politica envio y devolucion

--- Controllers/Politica_envioController.cs
+++ Controllers/Politica_envioController.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace UrbanX.Controllers
+namespace urbanx.Controllers
 {
 
     public class Politica_envioController : Controller
