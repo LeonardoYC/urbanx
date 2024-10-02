3d06ec0dc5dfd1ea8b58b142536eca9a0188b746
Leonardo <leonardoyupan2012@gmail.com>
2024-10-02 00:31:33 -0500

    acabado y reestructura del catalogo

--- Controllers/CatalogoController.cs
+++ Controllers/CatalogoController.cs
@@ -25,11 +25,54 @@ namespace urbanx.Controllers
             _userManager = userManager;
         }
 
-
-        public IActionResult Index()
+        public IActionResult Index(string? searchString)
         {
-            return View();
+            var productos = from o in _context.DataProducto select o;
+
+            if(!String.IsNullOrEmpty(searchString)){
+                productos = productos.Where(s => s.Nombre.Contains(searchString) || s.Categoria.Contains(searchString));
+            }
+
+            productos = productos.Where(l => l.Estado.Contains("Stock"));
+            return View(productos.ToList());
         }
+
+
+        public async Task<IActionResult> Details(int? id){
+            Producto objProduct = await _context.DataProducto.FindAsync(id);
+            if(objProduct == null){
+                return NotFound();
+            }
+            return View(objProduct);
+        }
+
+        public async Task<IActionResult> Add(int? id, string Talla = "S", int Cantidad = 1) // Talla por defecto
+        {
+            var userID = _userManager.GetUserName(User);
+            if (userID == null)
+            {
+                ViewData["Message"] = "Por favor debe loguearse antes de agregar un producto";
+                List<Producto> productos = new List<Producto>();
+                return View("Index", productos);
+            }
+            else
+            {
+                var producto = await _context.DataProducto.FindAsync(id);
+                Helper.SessionExtensions.Set<Producto>(HttpContext.Session, "MiUltimoProducto", producto);
+                Carrito carrito = new Carrito();
+                carrito.Producto = producto;
+                carrito.Precio = producto.Precio;
+                carrito.Talla = Talla; // Asignar la talla seleccionada (por defecto "S")
+                carrito.Cantidad = Cantidad;
+                carrito.UserID = userID;
+                _context.Add(carrito);
+                await _context.SaveChangesAsync();
+                ViewData["Message"] = "Se Agrego al carrito";
+                return RedirectToAction(nameof(Index));
+            }
+        }
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- Models/Producto.cs
+++ Models/Producto.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Diagnostics.CodeAnalysis;
 
-namespace UrbanX.Models
+namespace urbanx.Models
 {
     [Table("t_producto")]
     public class Producto
