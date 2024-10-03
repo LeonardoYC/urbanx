6eafabb18fb46fcde2f21ac7c5cdc3f81c5b6bf1
LeonardoYC <leonardoyupan2012@gmail.com>
2024-10-03 05:09:21 -0500

    arreglos

--- Models/Pago.cs
+++ Models/Pago.cs
@@ -1,10 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations.Schema;
+
 namespace urbanx.Models
 {
+    [Table("t_pago")]
     public class Pago
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
@@ -17,7 +15,7 @@ namespace urbanx.Models
         public string? DueDateYYMM { get; set; }
         [NotMapped]
         public string? Cvv { get; set; }
-        public Decimal MontoTotal { get; set; }
-        public string? UserID { get; set; }
+        public Decimal MontoTotal{ get; set; }
+        public string? UserID{ get; set; }
     }
 }
