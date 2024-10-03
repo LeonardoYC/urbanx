bdf35ea1f7733e7ca4edfc857d8efd8fd0bc4213
JeanClix <jp10.02.2003@gmail.com>
2024-10-03 02:04:48 -0500

    model Pago

--- Models/Pago.cs
+++ Models/Pago.cs
@@ -2,11 +2,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using System.ComponentModel.DataAnnotations.Schema;
 namespace urbanx.Models
 {
     public class Pago
     {
-        
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
+        public int Id { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public string? NombreTarjeta { get; set; }
+        public string? NumeroTarjeta { get; set; }
+        [NotMapped]
+        public string? DueDateYYMM { get; set; }
+        [NotMapped]
+        public string? Cvv { get; set; }
+        public Decimal MontoTotal { get; set; }
+        public string? UserID { get; set; }
     }
 }
