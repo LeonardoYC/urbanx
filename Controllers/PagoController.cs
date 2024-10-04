bce464e008226405c3438805d84bf2601778b134
LeonardoYC <leonardoyupan2012@gmail.com>
2024-10-03 22:48:23 -0500

    diseños finales

--- Controllers/PagoController.cs
+++ Controllers/PagoController.cs
@@ -5,23 +5,80 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using urbanx.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using urbanx.Data;
 
 namespace urbanx.Controllers
 {
-    [Route("[controller]")]
     public class PagoController : Controller
     {
         private readonly ILogger<PagoController> _logger;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly ApplicationDbContext _context;
 
-        public PagoController(ILogger<PagoController> logger)
+        public PagoController(ILogger<PagoController> logger,
+            UserManager<IdentityUser> userManager,
+            ApplicationDbContext context)
         {
             _logger = logger;
+            _userManager = userManager;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public IActionResult Create(Decimal monto)
         {
-            return View();
+
+            Pago pago = new Pago();
+            pago.UserID = _userManager.GetUserName(User);
+            pago.MontoTotal = monto;
+            return View(pago);
         }
+                [HttpPost]
+        public IActionResult Pagar(Pago pago)
+        {
+            pago.PaymentDate = DateTime.UtcNow;
+            _context.Add(pago);
+
+            var itemsCarrito = from o in _context.DataItemCarrito select o;
+            itemsCarrito = itemsCarrito.
+                Include(p => p.Producto).
+                Where(s => s.UserID.Equals(pago.UserID) && s.Estado.Equals("PENDIENTE"));
+
+            Pedido pedido = new Pedido();
+            pedido.UserID = pago.UserID;
+            pedido.Total = pago.MontoTotal;
+            pedido.pago = pago;
+            pedido.Status = "PENDIENTE";
+            _context.Add(pedido);
+
+            List<DetallePedido> itemsPedido = new List<DetallePedido>();
+            foreach(var item in itemsCarrito.ToList()){
+                DetallePedido detallePedido = new DetallePedido();
+                detallePedido.pedido=pedido;
+                detallePedido.Precio = item.Precio;
+                detallePedido.Producto = item.Producto;
+                detallePedido.Cantidad = item.Cantidad;
+                itemsPedido.Add(detallePedido);
+            }
+
+
+            _context.AddRange(itemsPedido);
+
+            foreach (Carrito p in itemsCarrito.ToList())
+            {
+                p.Estado="PROCESADO";
+            }
+
+            _context.UpdateRange(itemsCarrito);
+
+            _context.SaveChanges();
+
+            ViewData["Message"] = "El pago se ha registrado y su pedido nro "+ pedido.ID +" esta en camino";
+            return View("Create");
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
